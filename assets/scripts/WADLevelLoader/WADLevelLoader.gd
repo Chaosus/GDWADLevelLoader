4cfbef6c5bfd7aa24bc87a5c32577b86a003c092
Chaosus <chaosus89@gmail.com>
2018-02-01 14:54:42 +0300

    Preparations for new release 2

--- assets/scripts/WADLevelLoader/WADLevelLoader.gd
+++ assets/scripts/WADLevelLoader/WADLevelLoader.gd
@@ -11,13 +11,13 @@ export(String) var WADPath = "e1m1.wad"
 
 export(String) var LevelName = "E1M1"
 
-export(int, "Map", "Geometry") var Mode = 0
+export(int, "Map", "Geometry", "Overlay") var Mode = 0
+
+export(SpatialMaterial) var DefaultWallMaterial = null
 
 export(float) var Scale = 0.05
 
 export(bool) var PrintDebugInfo = true
-
-var SurfaceMaterial
 
 class Header:
 	var type
@@ -108,6 +108,29 @@ class Sector:
 	var tag = 0
 	var faceset_floor
 	var faceset_ceil
+	var height
+
+class FacesetSegment:
+	var v1
+	var v2
+	var a
+	var b
+	func _init(p_v1, p_v2, p_a, p_b):
+		v1 = p_v1
+		v2 = p_v2
+		a = p_a
+		b = p_b
+
+class Face:
+	var v1
+	var v2
+	var v3
+	var v4
+	func _init(p_v1,p_v2,p_v3,p_v4):
+		v1 = p_v1
+		v2 = p_v2
+		v3 = p_v3
+		v4 = p_v4
 
 class Faceset:
 	var vertices = []
@@ -117,6 +140,11 @@ class Faceset:
 	var uv = []
 	func _init(p_texture):
 		texture = p_texture
+	func add_segment(v1, v2, a, b):
+		segments.push_back(FacesetSegment.new(v1, v2, a, b))
+	func add_face(p_face, p_uv):
+		faces.push_back(p_face)
+		uv.push_back(p_uv)
 
 func decode_32_as_string(file):
 	var c1 = char(file.get_8())
@@ -151,10 +179,7 @@ func to_short(a, b):
 func combine_8_bytes_to_string(c1, c2, c3, c4, c5, c6, c7, c8):
 	return char(c1) + char(c2) + char(c3) + char(c4) + char(c5) + char(c6) + char(c7) + char(c8)
 
-func create_poly(geometry, linedef, side, sector, width, front_lower_left, front_lower_right, front_upper_right, front_upper_left):
-	geometry.begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
-	geometry.set_color(Color(1,1,1))
-	geometry.end()		
+
 
 func load_wad(wad_path, level_name, mode):
 	var buffer
@@ -190,14 +215,20 @@ func load_wad(wad_path, level_name, mode):
 	var lump_reject
 	var lump_blockmap
 	
-	var first = true
 	var breakAfter = false
+	var lumps_readed = false
+	var first = true
 	file.seek(header.dirOffset)
 	for i in range(header.lumpNum):
+		if breakAfter && lumps_readed:
+			break
+		
 		var lump = read_lump(file)
 		if first:
 			lump_mapname = lump
 			first = false
+		var name = lump.name
+		print(name)
 		match lump.name:
 			"THINGS":
 				lump_things = lump
@@ -219,8 +250,7 @@ func load_wad(wad_path, level_name, mode):
 				lump_reject = lump
 			"BLOCKMAP":
 				lump_blockmap = lump
-				if breakAfter:
-					break
+				lumps_readed = true
 			level_name:
 				breakAfter = true
 	if PrintDebugInfo:
@@ -352,15 +382,24 @@ func load_wad(wad_path, level_name, mode):
 	file.close()
 	
 	if PrintDebugInfo:
-		print("BUILDING GEOMETRY")
+		print("PREPARING GEOMETRY")
 	i = 1
+	var polygons = []
+	var polyvertexes = []
+	
+	var MapMaterial = SpatialMaterial.new()
+	MapMaterial.flags_unshaded = true
+	MapMaterial.flags_vertex_lighting = true
+	MapMaterial.vertex_color_use_as_albedo = true
+	
 	for ld in linedefs:	
-		var geometry = ImmediateGeometry.new()
+		
 		var vertex1 = vertexes[ld.start_vertex]
 		var vertex2 = vertexes[ld.end_vertex]
 		
-		if mode == 0: # Map mode
-			geometry.material_override = SurfaceMaterial
+		if mode == 0 or mode == 2: # Map mode or Overlay mode
+			var geometry = ImmediateGeometry.new()
+			geometry.material_override = MapMaterial
 			geometry.begin(Mesh.PRIMITIVE_LINES)
 			if ld.type != 0:
 				geometry.set_color(Color(1,1,0))
@@ -368,40 +407,89 @@ func load_wad(wad_path, level_name, mode):
 				geometry.set_color(Color(1,0,0))
 			geometry.add_vertex(Vector3(vertex1.x,0,vertex1.y))
 			geometry.add_vertex(Vector3(vertex2.x,0,vertex2.y))
-			geometry.end()
+			geometry.end()			
+			add_child(geometry)
 			
-		else: # Geometry mode
+		if mode == 1 or mode == 2: # Geometry mode or Overlay mode
 		
 			var width = sqrt((vertex1.x-vertex2.x)*(vertex1.x-vertex2.x) + (vertex1.y-vertex2.y)*(vertex1.y-vertex2.y))
 			
 			if ld.front != -1:
-				var side = sidedefs[ld.front]
-				var sector = sectors[side.sector]
+				var side1 = sidedefs[ld.front]
+				var sector1 = sectors[side1.sector]
 				
 				var front_lower_left = i
 				var front_upper_left = i+1
 				var front_lower_right = i+2
 				var front_upper_right = i+3
+				
+				polyvertexes.push_back(Vector3(vertex1.x, sector1.floor_height, vertex1.y)) # lower left
+				polyvertexes.push_back(Vector3(vertex1.x, sector1.ceil_height, vertex1.y)) # upper left
+				polyvertexes.push_back(Vector3(vertex2.x, sector1.floor_height, vertex2.y)) # lower right
+				polyvertexes.push_back(Vector3(vertex2.x, sector1.ceil_height, vertex2.y)) # upper right
 
-				if ld.flags != LDT_TWO_SIDED and side.middle_texture != '-':
-					create_poly(geometry, ld, side, sector, width, 
+				if ld.flags != LDT_TWO_SIDED and side1.middle_texture != '-':
+					polygons.push_back(create_poly(ld, side1, sector1, width, 
 					front_lower_left, front_lower_right,
-					front_upper_right, front_upper_left)
-			
-			geometry.material_override = SurfaceMaterial
-			
-		add_child(geometry)
-	
-func _ready():
-	
-	if SurfaceMaterial == null:
-		SurfaceMaterial = SpatialMaterial.new()
-		SurfaceMaterial.flags_unshaded = true
-		SurfaceMaterial.flags_vertex_lighting = true
-		SurfaceMaterial.vertex_color_use_as_albedo = true
-		SurfaceMaterial.params_cull_mode = SurfaceMaterial.CULL_DISABLED
+					front_upper_right, front_upper_left))
+					
+				sector1.faceset_floor.add_segment(vertex1, vertex2, front_lower_left, front_lower_right)
+				sector1.faceset_ceil.add_segment(vertex2, vertex1, front_upper_right, front_upper_left)
+				
+				i+=4
+				
+			if ld.back != -1:
+				var side2 = sidedefs[ld.back]
+				var sector2 = sectors[side2.sector]
+				
+				var back_lower_left = i
+				var back_upper_left = i+1
+				var back_lower_right = i+2
+				var back_upper_right = i+3	
+				
+				polyvertexes.push_back(Vector3(vertex1.x, sector2.floor_height, vertex1.y)) # lower left
+				polyvertexes.push_back(Vector3(vertex1.x, sector2.ceil_height, vertex1.y)) # upper left
+				polyvertexes.push_back(Vector3(vertex2.x, sector2.floor_height, vertex2.y)) # lower right
+				polyvertexes.push_back(Vector3(vertex2.x, sector2.ceil_height, vertex2.y)) # upper right
+				
+				if ld.flags != LDT_TWO_SIDED and side2.middle_texture != '-':
+					polygons.push_back(create_poly(ld, side2, sector2, width, 
+					back_lower_left, back_lower_right,
+					back_upper_right, back_upper_left))
+				
+				sector2.faceset_floor.add_segment(vertex2, vertex1, back_lower_right, back_lower_left)
+				sector2.faceset_ceil.add_segment(vertex1, vertex2, back_upper_left, back_upper_right)
+				
+				i+=4
+			#if ld.front != -1 and ld.back != -1 and ld.flags == LDT_TWO_SIDED:
+			#	pass
+				
+	if mode == 1 or mode == 2: # Geometry mode or Overlay mode	
+		var polycount = polygons.size()
+		for sector in sectors:
+			var geometry = ImmediateGeometry.new()
+			geometry.material_override = DefaultWallMaterial
+			for s in sector.faceset_ceil.segments:
+				geometry.begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
+				geometry.set_color(Color(1,1,1))
+				var height = sector.floor_height / 3 # (sector.ceil_height - sector.floor_height) / 10
+				geometry.set_uv(Vector2(0,0))
+				geometry.add_vertex(Vector3(s.v2.x, 0, s.v2.y))
+				geometry.set_uv(Vector2(1,0))
+				geometry.add_vertex(Vector3(s.v1.x, 0, s.v1.y))
+				geometry.set_uv(Vector2(0,1))
+				geometry.add_vertex(Vector3(s.v2.x, height, s.v2.y))			
+				geometry.set_uv(Vector2(1,1))
+				geometry.add_vertex(Vector3(s.v1.x, height, s.v1.y))
+				geometry.end()		
+			add_child(geometry)
 
+func create_poly(linedef, side, sector, width, lower_left, lower_right, upper_right, upper_left):
+	var polygon = Faceset.new(side.middle_texture)
+	var height = sector.ceil_height - sector.floor_height
+	sector.height = height / 1000.0
+	polygon.add_face(Face.new(lower_left, lower_right, upper_right, upper_left), Vector2(0,0))
+	return polygon
+
+func _ready():	
 	load_wad(WADPath, LevelName, Mode)
-	
-	if Mode == 1:
-		get_viewport().debug_draw = Viewport.DEBUG_DRAW_WIREFRAME
