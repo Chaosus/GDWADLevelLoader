a65aea1e6836e23a06e8e6f8620c54095ab9c1b7
Chaosus <chaosus89@gmail.com>
2018-02-10 21:28:06 +0300

    Writing from scratch 1

--- assets/scripts/WADLevelLoader/WADLevelLoader.gd
+++ assets/scripts/WADLevelLoader/WADLevelLoader.gd
@@ -6,7 +6,7 @@ extends Spatial
 
 # EXPORTS
 
-export(String) var WADPath = "e1m1.wad"
+export(String, FILE) var WADPath = "e1m1.wad"
 
 export(String) var LevelName = "E1M1"
 
@@ -98,13 +98,13 @@ enum {
 }	
 
 class Linedef:
-	var start_vertex
-	var end_vertex
+	var start
+	var end
 	var flags
 	var type
 	var trigger
-	var rsidenum
-	var lsidenum
+	var front
+	var back
 
 class Sidedef:
 	var x_offset
@@ -170,101 +170,147 @@ class Map:
 
 # CUSTOM GEOMETRY TYPES
 
-# SIDE TYPE
-enum {
-	ST_TOP,
-	ST_MIDDLE
-	ST_BOTTOM,
-	ST_MAX
-}
-
-# FLAT TYPE
-enum {
-	FT_FLOOR
-	FT_CEIL,
-	FT_MAX
-}
-
-class MapObject:
-	var mesh
-	var material
-
-class MapSide extends MapObject:
-	var wall = null
-	var upper_flat = null
-	var upper_index1 = -1
-	var upper_index2 = -1
-	var lower_flat = null
-	var lower_index1 = -1
-	var lower_index2 = -1
-	var peg_to_bottom = false
-
-class MapFlat extends MapObject:
-	var sides = []
-	var height
-	var prev_height
-
-class MapWall extends MapObject:
-	var flags
-	var lsides = []
-	var rsides = []
-
-class MapTriangle:
-	var v1
-	var v2
-	var v3
-	var ld1
-	var ld2
-	var ld3
-	var leftside1
-	var leftside2
-	var leftside3
-	var sector
-	func has_ld():
-		return ld1 != -1 || ld2 != -1 || ld3 != -1
-
-class Bounds:
-	var xMin
-	var yMin
-	var xMax
-	var yMax
+class Triangulation:
+	var map
+	func _init(p_map):
+		map = p_map
+		
+	func get_sector_sidedefs(sector):
+		var output = []
+		for sd in map.sidedefs:
+			if sd.sector == sector:
+				output.append(sd)
+		return output
+		
+	func get_sector_linedefs(sector):
+		var output = []
+		var sidedefs = get_sector_sidedefs(sector)
+		
+		for ld in map.linedefs:
+			if sidedefs.has(ld.front) or sidedefs.has(ld.back):
+				var fs = map.sidedefs[ld.front].sector
+				var bs
+				if ld.back != 0xFFFF:
+					bs = map.sidedefs[ld.back].sector
+				else:
+					bs = -1
+				if fs != bs:
+					output.append(ld)
+		
+		return output
+	
+	func vertex_to_v2(vertex):
+		return map.vertexes[vertex].get_v2()
+	
+	func line_angle(a, b):
+		return atan2(b.y - a.y, b.x - a.x)
+	
+	func trace_lines(sector):
+		var output = []
+		var lines = get_sector_linedefs(sector)
+		
+		var i = 0
+		var safe1 = 1000
+		var safe2 = 1
+		
+		var vertex_lines = {}
+		for l in lines:
+			if not vertex_lines.has(l.start):
+				vertex_lines[l.start] = 1
+			else:
+				vertex_lines[l.start] += 1
+			
+			if not vertex_lines.has(l.end):
+				vertex_lines[l.end] = 1
+			else:
+				vertex_lines[l.end] += 1
+				
+		var found_unclosed_sector = 0
+		
+		for e in vertex_lines:
+			if e < 2:
+				found_unclosed_sector += 1
+		
+		if found_unclosed_sector > 0:
+			print("unclosed_sector :", sector)
+			return null
+		
+		while lines.size() > 0 && safe1 > 0:
+			safe1-=1
+			
+			var trace = PoolIntArray()
+			var line = lines[0]
+			lines.remove(0)
+			
+			trace.append(line.start)
+			var next = line.end
+			
+			safe2 = 1000
+			while trace[0] != next && safe2 > 0:
+				safe2-=1
+				var connected_lines = PoolIntArray()
+				var connected_line_front = PoolByteArray()
+				for line in lines:
+					if line.start == next:
+						connected_lines.append(i)
+						connected_line_front.append(1)
+					elif line.end == next:
+						connected_lines.append(i)
+						connected_line_front.append(0)
+				
+				if connected_lines.size() == 1:
+					trace.append(next)
+					next = lines[connected_lines[0]].end if connected_line_front[0] == 1 else lines[connected_lines[0]].start
+					lines.remove(abs(connected_lines[0]))
+				else:
+					var v1 = vertex_to_v2(trace[trace.size()-1])
+					var v2 = vertex_to_v2(next)
+					var point_index = lines[connected_lines[0]].end if connected_line_front[0] == 1 else lines[connected_lines[0]].start
+					var v3 = vertex_to_v2(point_index)
+					var min_angle = abs(line_angle(v1, v2) - line_angle(v3, v2))
+					var min_index = 0
+					i = 1
+					for i in range(1, connected_lines.size()):
+						point_index = lines[connected_lines[i]].end if connected_line_front[i] else lines[connected_lines[i]].start
+						v3 = vertex_to_v2(point_index)
+						var new_angle = abs(line_angle(v1, v2) - line_angle(v3, v2))
+						if new_angle < min_angle:
+							min_index = i
+							min_angle = new_angle
+						i+=1
+					trace.append(next)
+					next = lines[connected_lines[min_index]].end if connected_line_front[min_index] else lines[connected_lines[min_index]].start
+					lines.remove(abs(connected_lines[min_index]))
+			
+			var v2_trace = PoolVector2Array()
+			for v in trace:
+				v2_trace.append(vertex_to_v2(v))
+			
+			output.append(v2_trace)
+		
+		if safe1 <= 0:
+			print("first while loop exceeded limit!")
+		if safe2 <= 0:
+			print("second while loop exceeded limit! Sector: ", sector, " Lines left: ", lines.size())
+		
+		return output
+		
+	func triangulate(sector):
+		var polygons = trace_lines(sector)
+		
+		if polygons == null:
+			return null
+		
+		print("SUCCESS !")
+		
+		return polygons
 
 # VARIABLES
 
 var map = null
-var flats = []
-var walls = []
-var triangles = []
 var map_material
 
 # FUNCTIONS
-
-# <finished>
-func get_flat_index(sector_index, is_ceiling):
-	return sector_index * FT_MAX + (FT_CEIL if is_ceiling else FT_FLOOR)
-
-# <finished>
-func get_flat(sector_index, is_ceiling):
-	return flats[get_flat_index(sector_index, is_ceiling)]
-
-# <finished>
-func get_sector_linedef_count(sector_index):
-	if sector_index == -1:
-		return 0
-	var count = 0
-	var i = 0
-	while i < map.linedefs.size():
-		var ld = map.linedefs[i]
-		var rsector = map.sidedefs[ld.rsidenum].sector if ld.rsidenum != -1 else -1
-		var lsector = map.sidedefs[ld.lsidenum].sector if ld.lsidenum != -1 else -1
-		if rsector == sector_index || lsector == sector_index:
-			count+=1
-		i+=1
-	return count
-
-# <finished>
-func is_sector_degenerative(sector_index):
-	return get_sector_linedef_count(sector_index) < 3
 
 # <almost_finished>
 func load_wad(wad_path, level_name, mode):
@@ -370,13 +416,13 @@ func load_wad(wad_path, level_name, mode):
 	i = 0
 	while i < buffer.size():
 		var linedef = Linedef.new()
-		linedef.start_vertex = to_short(buffer[i],buffer[i+1])
-		linedef.end_vertex = to_short(buffer[i+2],buffer[i+3])
+		linedef.start = to_short(buffer[i],buffer[i+1])
+		linedef.end = to_short(buffer[i+2],buffer[i+3])
 		linedef.flags = to_short(buffer[i+4],buffer[i+5])
 		linedef.type = to_short(buffer[i+6],buffer[i+7])
 		linedef.trigger = to_short(buffer[i+8],buffer[i+9])
-		linedef.rsidenum = to_short(buffer[i+10],buffer[i+11])
-		linedef.lsidenum = to_short(buffer[i+12],buffer[i+13])
+		linedef.front = to_short(buffer[i+10],buffer[i+11])
+		linedef.back = to_short(buffer[i+12],buffer[i+13])
 		map.linedefs.push_back(linedef)
 		i+=14
 	
@@ -472,9 +518,6 @@ func load_wad(wad_path, level_name, mode):
 	map_material.flags_vertex_lighting = true
 	map_material.vertex_color_use_as_albedo = true
 	
-	flats.resize(map.sectors.size() * 2)
-	walls.resize(map.linedefs.size())
-	
 	match mode:
 		0:
 			build_map_geometry()
@@ -505,400 +548,32 @@ func load_wad(wad_path, level_name, mode):
 #			add_child(geometry)
 
 
+class SectorPolygon:
+	var points = []
+	var triangles = []
+	
+	func get_offset_triangles(offset):
+		var output = []
+		for t in triangles:
+			output.append(t + offset)
+		return output
+	
+	func points_to_v3(z):
+		var vectors = []
+		for p in points:
+			vectors.append(Vector3(p.x, z, p.y))
+		return vectors
+
 # <unfinished>
 func build_level_geometry():
-	
-	triangulate()
-	finish_triangles()
-	
-	for i in range(map.sectors.size()):
-		build_sector(i)
-	
-	for i in range(map.linedefs.size()):
-		build_wall(i)
-
-# <finished>
-func add_point_to_rect(rect, point):
-	rect.xMin = min(rect.xMin, point.x)
-	rect.yMin = min(rect.yMin, point.y)
-	rect.xMax = max(rect.xMax, point.x)
-	rect.yMax = max(rect.yMax, point.y)
-	return rect
-
-# <finished>
-func get_bbox():
-	var bbox = Bounds.new()
-	bbox.xMin = INF
-	bbox.yMin = INF
-	bbox.xMax = -INF
-	bbox.yMax = -INF
-	
-	for v in map.vertexes:
-		bbox = add_point_to_rect(bbox, v)
-	
-	return bbox
-
-# <finished>
-func get_taper_rect(rect, offset):
-	rect.xMin += offset
-	rect.yMin += offset
-	rect.xMax -= offset
-	rect.yMax -= offset
-	return rect
-
-class LinedefResult:
-	var index
-	var left_side
-
-func find_linedef_by_vertices(v1, v2):
-	var result = LinedefResult.new()
-	result.index = -1
-	for i in range(map.linedefs.size()):
-		var ld = map.linedefs[i]
-		if ld.start_vertex == v1 && ld.end_vertex == v2:
-			result.left_side = true
-			result.index = i
-			return result
-		if ld.start_vertex == v2 && ld.end_vertex == v1:
-			result.left_side = false
-			result.index = i
-	return result
-	
-
-# <unfinished>
-func triangulate():
-	var bbox = get_bbox()
-	bbox = get_taper_rect(bbox, -500.0)	
-
-# <finished>
-func get_sector_by_linedef(ld_index, left_side, only_non_degenerative):
-	if ld_index == -1:
-		return -1
-	
-	var ld = map.linedefs[ld_index]
-	var sidedef_index = ld.lsidenum if left_side else ld.rsidenum
-	
-	if sidedef_index == -1:
-		return -1
+	for i in range(map.sectors.size()):	
+		var st = Triangulation.new(map)
+		st.triangulate(i)
 		
-	var sector_index = map.sidedefs[sidedef_index].sector
-	if sector_index == -1:
-		return -1
-	
-	if only_non_degenerative:
-		if is_sector_degenerative(sector_index):
-			return -1
-	
-	return sector_index
-
-# <finished>
-func get_sector_by_triangle(triangle):
-	var sector_index = 0
-	sector_index = get_sector_by_linedef(triangle.ld3, triangle.leftside3, true)
-	if sector_index != -1:
-		return sector_index
-	
-	sector_index = get_sector_by_linedef(triangle.ld2, triangle.leftside2, true );
-	if sector_index != -1:
-		return sector_index;
-	
-	sector_index = get_sector_by_linedef(triangle.ld1, triangle.leftside1, true );
-	if sector_index != -1:
-		return sector_index;
-	
-	return -1
-
-# <finished>
-func find_triangle_by_vertices(triangle_indices, v1, v2):
-	for i in triangle_indices:
-		var t = triangles[i]
-		
-		# CW winding
-		if (t.v1 == v1 && t.v2 == v2) || (t.v2 == v1 && t.v3 == v2) || (t.v3 == v1 && t.v1 == v2):
-			return i
-		
-		# CCW winding
-		if (t.v1 == v2 && t.v2 == v1) || (t.v2 == v2 && t.v3 == v1) || (t.v3 == v2 && t.v1 == v1):
-			return i
-	return -1
-
-# <need_test>
-func find_sector_by_triangle(triangle_index):
-	var availablelist = []
-	var checklist = []
-	
-	for i in range(triangles.size()):
-		if i != triangle_index:
-			availablelist.push_back(i)
-	
-	checklist.push_back(triangle_index)
-	
-	var max_iterations = 32
-	var iteration = 0
-	
-	while availablelist.size() > 0:
-		for i in checklist:
-			var t = triangles[i]
-			if t.has_ld():
-				return get_sector_by_triangle(t)
-	
-		var neightbourlist = [] # to PoolIntArray
-		for i in checklist:
-			var t = triangles[i]
-			for j in range(3):
-				var index1 = t.v1 if j == 0 else t.v2 if j == 1 else t.v3
-				var index2 = t.v2 if j == 0 else t.v3 if j == 1 else t.v1
-				var n = find_triangle_by_vertices(availablelist, index1, index2)
-				if n != -1:
-					neightbourlist.push_back(n)
-					availablelist.remove(n)
-		checklist = neightbourlist
-		iteration+=1
-		if iteration >= max_iterations:
-			break
-	
-	return -1
-	
-# <unfinished>
-func finish_triangles():
-	
-#	for i in range(0, map.linedefs.size()): # pointSet.Triangles[ i ];
-#		var ld = map.linedefs[i]
-#		var triangle = MapTriangle.new()
-#
-#		triangle.v1 = 0
-#		triangle.v2 = 0
-#		triangle.v3 = 0
-#
-#		var ld1 = find_linedef_by_vertices(triangle.v1, triangle.v2)
-#		var ld2 = find_linedef_by_vertices(triangle.v2, triangle.v3)
-#		var ld3 = find_linedef_by_vertices(triangle.v3, triangle.v1)
-#
-#		triangle.ld1 = ld1.index
-#		triangle.ld2 = ld2.index
-#		triangle.ld3 = ld3.index
-#
-#		triangle.leftside1 = ld1.left_side
-#		triangle.leftside2 = ld2.left_side
-#		triangle.leftside3 = ld3.left_side
-#
-#		triangle.sector = get_sector_by_triangle(triangle)
-#		triangles.push_back(triangle)
-#		print(str(i))
-	
-	for i in range(triangles.size()):
-		var t = triangles[i]
-		if t.sector == -1:
-			t.sector = find_sector_by_triangle(i)
-			triangles[i] = t
-			
-	pass
-
-# <finished>
-func build_sector(sector_index):
-	if is_sector_degenerative(sector_index):
-		return
-	var sector = map.sectors[sector_index]
-	
-	var sector_triangles = []
-	var vertex_indices = []
-	
-	# choose sector triangles
-	for t in triangles:
-		if t.sector == sector_index:
-			sector_triangles.push_back(t)
-			vertex_indices.push_back(t.v1)
-			vertex_indices.push_back(t.v2)
-			vertex_indices.push_back(t.v3)
-	
-	# build sector vertices array
-	var sector_vertices = []
-	for i in range(vertex_indices.size()):
-		sector_vertices.push_back(SHORT2FLOAT * map.vertices[vertex_indices[i]].get_v2())
-	
-	# map triangle indexes from global array to local
-	for i in range(sector_triangles.size()):
-		var t = sector_triangles[i]
-		t.v1 = vertex_indices.find(t.v1)
-		t.v2 = vertex_indices.find(t.v2)
-		t.v3 = vertex_indices.find(t.v3)
-		sector_triangles[i] = t
-	
-	# create floor and ceiling flats
-	build_sector_flat(sector_index, vertex_indices, sector_vertices, sector_triangles, false)
-	build_sector_flat(sector_index, vertex_indices, sector_vertices, sector_triangles, true)
-
 # <unfinished>
 func create_material(texture_name, is_transparent):
 	var material = SpatialMaterial.new()
 	return material
-
-# <unfinished>
-func build_sector_flat(sector_index, vertex_indices, sector_vertices, sector_triangles, is_ceiling):
-	var sector = map.sectors[sector_index]
-	var texture_name = sector.ceil_texture if is_ceiling else sector.floor_texture
-	var is_sky = texture_name == SKYTEXNAME
-	var brightness = sector.light_level / SHORTMAXVALUE
-	var color = Color(brightness, brightness, brightness, 1.0)
-	var uv_tiling = PIXELS_PER_UNIT / PIXELS_PER_FLAT
-	var material = create_material(texture_name, false)
-	var height = SHORT2FLOAT * (sector.ceil_height if is_ceiling else sector.floor_height)
-	var mesh = ImmediateGeometry.new()
-	mesh.name = "sector_" + str(sector_index) + "_" + ("ceiling" if is_ceiling else "floor")
-	
-	var flat_vertices = []
-	var flat_colors = []
-	var flat_triangle_indices = []
-	var flat_uv = []
-	
-	var center = Vector3()
-	for v in sector_vertices:
-		center += Vector3(v.x, 0.0, v.y)
-	center /= float(sector_vertices.size())
-	center.y = height
-	
-	if is_nan(center.x) || is_nan(center.z):
-		pass
-	
-	for v in sector_vertices:
-		flat_vertices.push_back(Vector3(v.x, height, v.y) - center)
-		flat_colors.push_back(color)
-		flat_uv.push_back = v * uv_tiling
-	
-	for i in range(sector_triangles.size()):
-		var t = sector_triangles[i]
-		if is_ceiling:
-			flat_triangle_indices[i * 3] = t.v1
-			flat_triangle_indices[i * 3 + 1] = t.v2
-			flat_triangle_indices[i * 3 + 2] = t.v3
-		else:
-			flat_triangle_indices[i * 3] = t.v1
-			flat_triangle_indices[i * 3 + 1] = t.v3
-			flat_triangle_indices[i * 3 + 2] = t.v2
-	
-	mesh.begin(Mesh.PRIMITIVE_TRIANGLES)
-	for i in range(flat_vertices.size()):
-		mesh.set_color(flat_colors[i])
-		mesh.set_uv(flat_uv[i])
-		mesh.add_vertex(flat_vertices[i])
-	mesh.end()
-	
-	add_child(mesh)
-	
-	var flat_index = get_flat_index(sector_index, is_ceiling)
-	var flat = MapFlat.new()
-	flat.mesh = mesh
-	flat.material = material
-	flats[flat_index] = flat
-	
-	
-# <finished>
-func build_wall(ld_index):
-	
-	var ld = map.linedefs[ld_index]	
-		
-	var v1 = ld.start_vertex
-	var v2 = ld.end_vertex
-		
-	var twoSided = ld.flags & LDF_TWO_SIDED
-	var topUnpegged = ld.flags & LDF_UPPER_UNPEGGED
-	var bottomUnpegged = ld.flags & LDF_LOWER_UNPEGGED
-	
-	var wall = MapWall.new()
-	wall.flags = ld.flags
-	
-	if twoSided:		
-		var rsd = map.sidedefs[ld.rsidenum]
-		var lsd = map.sidedefs[ld.lsidenum]
-		
-		var rs = map.sectors[rsd.sector]
-		var ls = map.sectors[lsd.sector]
-		
-		var rfloorh = SHORT2FLOAT * rs.floor_height
-		var rceilh = SHORT2FLOAT * rs.ceil_height
-		
-		var lfloorh = SHORT2FLOAT * ls.floor_height
-		var lceilh = SHORT2FLOAT * ls.ceil_height
-	
-		var upperFloor = get_flat(lsd.sector, false) if rfloorh < lfloorh else get_flat(rsd.sector, false)
-		var lowerCeiling = get_flat(lsd.sector, true) if lceilh < rceilh else get_flat(rsd.sector, true)
-		
-		# VALIDATE SIDEDEF TEXTURES
-		
-		if rfloorh != lfloorh:
-			if rsd.lower_texture == "-":
-				rsd.lower_texture = lsd.lower_texture
-			elif lsd.lower_texture == "-":
-				lsd.lower_texture = rsd.lower_texture
-		
-		if rceilh != lceilh:
-			if rsd.upper_texture == "-":
-				rsd.upper_texture = lsd.upper_texture
-			elif lsd.upper_texture == "-":
-				lsd.upper_texture = rsd.upper_texture
-		
-		# TOP WALL
-		
-		if rceilh > lceilh:
-			build_wall_side(ld.rsidenum, ld.start_vertex, ld.end_vertex, 
-			lceilh, rceilh,
-			ST_TOP, twoSided, !topUnpegged, wall, false,
-			get_flat(rsd.sector, true), get_flat(lsd.sector, true))
-		elif lceilh > rceilh:
-			build_wall_side(ld.lsidenum, ld.end_vertex, ld.start_vertex,
-			rceilh, lceilh,
-			ST_TOP, twoSided, !topUnpegged, wall, true,
-			get_flat(lsd.sector, true), get_flat(rsd.sector, true))
-			
-		# MIDDLE WALL
-		
-		if rsd.middle_texture != "-":
-			build_wall_side(ld.rsidenum, ld.start_vertex, ld.end_vertex,
-			upperFloor.height, lowerCeiling.height, ST_MIDDLE, twoSided, bottomUnpegged,
-			wall, false, lowerCeiling, upperFloor)
-		
-		if lsd.middle_texture != "-":
-			build_wall_side(ld.lsidenum, ld.end_vertex, ld.start_vertex,
-			upperFloor.height, lowerCeiling.height, ST_MIDDLE, twoSided, bottomUnpegged,
-			wall, true, lowerCeiling, upperFloor)
-		
-		# BOTTOM WALL
-			if rfloorh < lfloorh:
-				build_wall_side(ld.rsidenum, ld.start_vertex, ld.end_vertex,
-				rfloorh, lfloorh, ST_BOTTOM, twoSided, bottomUnpegged, wall, false,
-				get_flat(lsd.sector, false), get_flat(rsd.sector, false))
-			elif lfloorh < rfloorh:
-				build_wall_side(ld.lsidenum, ld.end_vertex, ld.start_vertex,
-				lfloorh, rfloorh, ST_BOTTOM, twoSided, bottomUnpegged, wall, true,
-				get_flat(rsd.sector, false), get_flat(lsd.sector, false))
-	else: # ONE_SIDED
-		var isLeftSide = false
-		var sd_index = ld.rsidenum
-		if sd_index == -1:
-			isLeftSide = true
-			sd_index =ld.lsidenum
-			# SWAP
-			var temp = v1
-			v1 = v2
-			v2 = temp
-		var sd = map.sidedefs[sd_index]
-		var s  = map.sectors[sd.sector]
-		
-		var floorh = SHORT2FLOAT * s.floor_height
-		var ceilh = SHORT2FLOAT * s.ceil_height
-		
-		build_wall_side(ld.rsidenum, v1, v2, floorh, ceilh, ST_MIDDLE, twoSided, bottomUnpegged, wall, isLeftSide,
-		get_flat(sd.sector, true), get_flat(sd.sector, false))
-
-# <unfinished>
-func build_wall_side(sd_index, v_index1, v_index2, floor_h, ceil_h, type, two_sided, peg_to_bottom, wall, is_left_side, upper_flat, lower_flat):
-	
-	if sd_index == -1:
-		return
-		
-	var v1 = SHORT2FLOAT * map.vertexes[v_index1].get_v2()
-	var v2 = SHORT2FLOAT * map.vertexes[v_index2].get_v2()
-	var sd = map.sidedefs[sd_index]
 
 # <almost_finished>
 func build_map_geometry():
@@ -907,8 +582,8 @@ func build_map_geometry():
 	map_spatial.name = "Map"
 	add_child(map_spatial)
 	for ld in map.linedefs:
-		var v1 = ld.start_vertex
-		var v2 = ld.end_vertex
+		var v1 = ld.start
+		var v2 = ld.end
 		
 		var vertex1 = map.vertexes[v1]
 		var vertex2 = map.vertexes[v2]
